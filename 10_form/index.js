1fe35fdde11b7c515e18a9d594e54002e9e4943e
keeemhs <alenen0227@naver.com>
2023-08-01 16:50:33 +0900

    pattern 속성의 정규식

--- 10_form/index.js
+++ 10_form/index.js
@@ -6,12 +6,29 @@ const PORT = 9000;
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 // view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 // router
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
+    // console.log(req.body);
+    // res.send('hello');
+    res.render('index', { title: '폼 전송 실습' });
+});
+app.get('/getForm', (req, res) => {
+    console.log(req.query);
+    res.render('result', {
+        title: "GET요청 폼 결과 확인하기",
+        userInfo: req.query
+    })
+});
+
+app.post('/postForm', (req, res) => {
     console.log(req.body);
+    res.render('result', {
+        title: 'POST요청 폼 결과 확인하기',
+        userInfo: req.body,
+    });
 })
 
 app.listen(PORT, () => {
