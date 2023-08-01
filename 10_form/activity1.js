3a2b9511d99fe87273b6d47b1787f7fecf72e65f
keeemhs <alenen0227@naver.com>
2023-08-01 17:38:17 +0900

    backend get post

--- 10_form/activity1.js
+++ 10_form/activity1.js
@@ -11,21 +11,19 @@ app.set('views', './views');
 
 // router
 app.get('/', (req, res) => {
-    // console.log(req.body);
-    // res.send('hello');
-    res.render('index', { title: '폼 전송 실습' });
+    res.render('activity1', { title: '실습1' });
 });
-// app.get('/getForm', (req, res) => {
-//     console.log(req.query);
-//     res.render('result', {
-//         title: "GET요청 폼 결과 확인하기",
-//         userInfo: req.query
-//     })
-// });
+app.get('/getForm', (req, res) => {
+    console.log(req.query);
+    res.render('activityresult1', {
+        title: "GET요청 폼 결과 확인하기",
+        userInfo: req.query
+    })
+});
 
 app.post('/postForm', (req, res) => {
     console.log(req.body);
-    res.render('result', {
+    res.render('activityresult1', {
         title: 'POST요청 폼 결과 확인하기',
         userInfo: req.body,
     });
