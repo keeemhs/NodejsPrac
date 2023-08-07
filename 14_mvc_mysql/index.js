1cf38d9beee1b34b9bc88fa27f8b0947d94cd1e2
keeemhs <alenen0227@naver.com>
2023-08-07 20:06:25 +0900

    mysql connect

--- 14_mvc_mysql/index.js
+++ 14_mvc_mysql/index.js
@@ -1,14 +1,56 @@
 const express = require('express');
 const app = express();
 const PORT = 9000;
+const mysql = require('mysql');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-const indexRouter = require('./routes')
-app.use('/', indexRouter);
+// mysql 연결
+const conn = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'keeemhs',
+    password: '',
+    database: 'kdt9',
+    port: 3306,
+});
+
+conn.connect( (err) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log('connect')
+});
+
+// const indexRouter = require('./routes')
+// app.use('/', indexRouter);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+// GET /visitor 방명록 전체 보이기
+app.get('/visitor', (req, res) => {
+    res.render('visitor');
+});
+// GET /visitor/get 방명록 하나 조회
+app.get('/visitor/get', (req, res) => {
+    res.send('방명록 하나조회');
+});
+// POST /visitor/write 방명록 하나 생성
+app.post('/visitor/write', (req, res) => {
+    res.send('방명록 하나 생성');
+});
+// PATCH /visitor/edit 방명록 하나 수정
+app.patch('/visitor/edit', (req, res) => {
+    res.send('방명록 하나 수정')
+});
+// DELETE /visitor/delete 방명록 하나 삭제
+app.delete('/visitor/delete', (req, res) => {
+    res.send('방명록 하나 삭제')
+});
 
 app.use*'*', (req, res) => {
     res.render('404');
--- 14_mvc_mysql/model/Visitor.js
+++ 14_mvc_mysql/model/Visitor.js
@@ -1,4 +1,4 @@
-exports.getVisitors = () => {
+ exports.getVisitors = () => {
     return [
         { id: 1, name: '홍길동', comment: '내가 왔다' },
         { id: 2, name: '이찬혁', comment: '으라차차' },
