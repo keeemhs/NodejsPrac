e09b97620c2b7385b44e3c1fd6e130c790003b56
keeemhs <alenen0227@naver.com>
2023-08-07 15:27:33 +0900

    230807 modelcomment

--- 14_mvc/routes/index.js
+++ 14_mvc/routes/index.js
@@ -1,48 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controller/CComment')
 
-// 임시 데이터
-const comments = [
-    {
-        id: 1,
-        userId: 'helloworld',
-        date: '2023-08-01',
-        comment: '안녕하세요!'
-    },
-    {
-        id: 2,
-        userId: 'happy',
-        date: '2023-08-02',
-        comment: '반갑습니다!'
-    },
-    {
-        id: 3,
-        userId: 'lucky',
-        date: '2023-08-03',
-        comment: '행복하세요!'
-    },
-    {
-        id: 4,
-        userId: 'good',
-        date: '2023-08-04',
-        comment: '좋은저녁되세요!'
-    },
-]
-
-router.get('/', (req, res) => {
-    res.render('index');
-});
+router.get('/', controller.main);
 //GET /comments
-router.get('/comments', (req, res) => {
-    res.render('comments', { commentInfos: comments });
-});
+router.get('/comments', controller.comments);
 //GET /comment/:id
-router.get('/comment/:id', (req, res) => {
-    // console.log(req.params);
-    // console.log(req.params.id);
-    const commentId = req.params.id;
-    console.log(comments[commentId - 1]);
-    res.render('comment', { commentInfo: comments[commentId - 1] });
-});
+router.get('/comment/:id', controller.comment);
 
 module.exports = router;
