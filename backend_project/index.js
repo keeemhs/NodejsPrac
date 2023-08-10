1b102073c89e99cbae520f7ec1e1b7ccef05aa50
keeemhs <alenen0227@naver.com>
2023-08-10 20:36:52 +0900

    join practice

--- backend_project/index.js
+++ backend_project/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+const db = require('./models');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const 
-
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+    res.render('index');
 });
+const studentRouter = require('./routes/student');
+app.use('/student', studentRouter);
+
+db.sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`http://localhost:${PORT}`);
+    });
+})
