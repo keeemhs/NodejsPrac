7e0a4b91f4cf0203fa5d0906d6a57240b981d6ae
keeemhs <alenen0227@naver.com>
2023-08-18 17:10:32 +0900

    secret practice

--- 20_secret/index.js
+++ 20_secret/index.js
@@ -1,40 +1,107 @@
 const express = require('express');
-const crypto = require('crypto');
 const app = express();
 const PORT = 8000;
-let pass = '';
-const salt = crypto.randomBytes(16).toString('hex'); //솔트생성
-const leng = 1000; //반복횟수
-const key = 64; //생성할 키의 길이
-const algo = 'sha512';
+require('dotenv').config();
 
+let hash = '';
+
+//ejs
+app.set('view engine', 'ejs');
+//body-parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post('/login', (req, res) => {
-    const { pw } = req.body;
-    //createHash: 지정한 알고리즘을 이용하여 해시 생성
-    //const pass = crypto.createHash('sha512').update(pw).digest('base64');
-    //pdkdf2(Sync):(Sync가 붙으면 동기): 비밀번호 기반 키도출함수
-    pass = crypto.pbkdf2Sync(pw, salt, leng, key, algo).toString('base64');
-    res.send(pass);
+//router
+app.get('/', (req, res) => {
+    console.log(process.env.NAME);
+    console.log(process.env.NODE_ENV);
+    res.render('index');
 });
-
+app.post('/hash', (req, res) => {
+    const { pw } = req.body;
+    // const hash = createHashPassword(pw);
+    // hash = createPbkdf(pw);
+    hash = bcryptPassword(pw);
+    res.json({ hash });
+});
 app.post('/verify', (req, res) => {
     const { pw } = req.body;
-    const compare = crypto.pbkdf2Sync(pw, salt, leng, key, algo);
-    console.log(compare);
-    //기본적인 방법
-    // if (compare === pass) {
-    //     res.send(true);
-    // } else {
-    //     res.send(false);
-    // }
-    //timingSafeEqual: 두개의 버퍼를 상수시간으로 비교하는 함수
-    const result = crypto.timingSafeEqual(compare, Buffer.from(pass, 'base64'));
-    res.send(result);
+    // const compare = verifyPassword(pw, salt, hash)
+    const compare = comparePassword(pw, hash);
+    res.json({ compare });
+});
+app.post('/cipher' , (req, res) => {
+    const { data } = req.body;
+    const encrypt = cipherEncrypt(data);
+    console.log('encrypt', encrypt);
+    const decrypt = decipher(encrypt);
+    res.json({ decrypt });
 });
 
+//server open
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
+
+//////////////// 암호와 /////////////////
+// 단방향
+const crypto = require('crypto');
+// 해쉬함수
+const createHashPassword = (password) => {
+    // createHash(알고리즘).update(암호화할값).digest(인코딩방식)
+    return crypto.createHash('sha512').update(password).digest('base64');
+};
+// pbkdf2함수
+const salt = crypto.randomBytes(16).toString('base64'); // 솔트 생성
+const interations = 100; // 반복 횟수
+const keylen = 64; // 생성할 키의 길이
+const digest = 'sha512'; // 해시 알고리즘
+
+// 단방향 암호화 생성
+const createPbkdf = (password) => {
+    // pbkdf2함수(비밀번호, 솔트, 반복횟수, 키의길이, 알고리즘)으로 생성이 되고
+    // 반환되는 값은 Buffer값
+    const hash = crypto.pbkdf2Sync(password, salt, interations, keylen, digest);
+    // console.log(hash);
+    return hash.toString('base64');
+};
+// 암호 비교
+const verifyPassword = (password, salt, dbPassword) => {
+    const compare = crypto.pbkdf2Sync(password, salt, interations, keylen, digest).toString('base64');
+    if( compare === dbPassword ) {
+        return true;
+    } else {
+        return false;
+    }
+};
+// 양방향
+
+const algorithm = 'aes-256-cbc'; // 256비트 크기를 사용, 블록크기는 128비트
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16); // 초기화 벡터, 데이터블록을 암호화할때 보안성을 높이기 위해 사용
+// 암호화
+const cipherEncrypt = (word) => {
+    const cipher = crypto.createCipheriv(algorithm, key, iv); // 암호화 객체 생성
+    let encryptedData = cipher.update(word, 'utf-8', 'base64'); // 암호화할 데이터 처리
+    encryptedData += cipher.final('base64'); // 최종 결과 생성
+    return encryptedData;
+};
+// 복호화
+const decipher = (encryptedData) => {
+    const decipher = crypto.createDecipheriv(algorithm, key, iv); // 복호화 객체 생성
+    let decryptedData = decipher.update(encryptedData, 'base64', 'utf-8'); // 복호화할 데이터 처리
+    decryptedData += decipher.final('utf-8');
+    return decryptedData;
+};
+
+// bcrypt(단방향)
+const bcrypt = require('bcrypt');
+const saltNumber = 10;
+// 암호화
+const bcryptPassword = (password) => {
+    return bcrypt.hashSync(password, saltNumber);
+};
+// 비교
+const comparePassword = (password, dbPassword) => {
+    return bcrypt.compareSync(password, dbPassword);
+};
