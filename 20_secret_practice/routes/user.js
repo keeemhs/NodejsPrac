214f06eef1a02df6a7a129a4589a7929ff60ef28
keeemhs <alenen0227@naver.com>
2023-08-20 11:15:53 +0900

    20_secret_activity answer

--- 20_secret_practice/routes/user.js
+++ 20_secret_practice/routes/user.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const controller = require('../controller/user');
 
 router.get('/', controller.index);
-//로그인
+router.get('/register', controller.get_register);
+router.get('/login', controller.get_login);
 //회원가입
+router.post('/register', controller.register);
+//로그인
+router.post('/login', controller.login);
 
 module.exports = router;
